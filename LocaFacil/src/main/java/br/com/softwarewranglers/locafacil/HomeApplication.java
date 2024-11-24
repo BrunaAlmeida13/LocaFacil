3f4c879f17e5023948368baa3abfd7c75e61ae5b
Eduardo De Lucca <eduardo.luccati@gmail.com>
2024-11-24 19:00:22 -0300

    Configuracoes Iniciais - Finalizado

--- LocaFacil/src/main/java/br/com/softwarewranglers/locafacil/HomeApplication.java
+++ LocaFacil/src/main/java/br/com/softwarewranglers/locafacil/HomeApplication.java
@@ -4,20 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HomeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/br/com/softwarewranglers/locafacil/view/home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("/br/com/softwarewranglers/locafacil/view/home-view.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
 
         stage.setMaximized(true);
-        stage.setTitle("Software Wranglers");
+        stage.setTitle("Loca Fácil - Software Wranglers");
         stage.setScene(scene);
         stage.show();
     }
